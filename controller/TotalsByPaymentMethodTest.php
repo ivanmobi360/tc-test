81ad5037b73e8ab51148a59fb87909e331cbbd97
irodriguez <irodriguez@mobi360.ca>
2013-07-18 11:56:59 -0500

    trying to fix errors in this report but needs review.

--- controller/TotalsByPaymentMethodTest.php
+++ controller/TotalsByPaymentMethodTest.php
@@ -17,15 +17,18 @@ class TotalsByPaymentMethodTest extends DatabaseBaseTest{
     $seller = $this->createUser('seller');
     $loc = $this->createLocation();
     $evt = $this->createEvent('First', $seller->id, $loc->id);
+    $this->setEventId($evt, 'aaa');
     $catA = $this->createCategory('CatA', $evt->id, 10.00);
     $catB = $this->createCategory('CatB', $evt->id, 4.00);
     
     $seller = $this->createUser('seller2');
+    $this->setEventId($evt, 'bbb');
     $evt = $this->createEvent('Second', $seller->id, $loc->id);
     $cat2 = $this->createCategory('Cat2', $evt->id, 15.00);
     
     $seller = $this->createUser('seller3');
     $evt = $this->createEvent('Third', $seller->id,  $loc->id);
+    $this->setEventId($evt, 'ccc');
     $this->setPaymentMethod($evt, self::OUR_CREDIT_CARD);
     $cat3 = $this->createCategory('Cat3', $evt->id, 20.00);
     
@@ -34,22 +37,22 @@ class TotalsByPaymentMethodTest extends DatabaseBaseTest{
     $client = new WebUser($this->db);
     $client->login($foo->username);
     
-    $client->addToCart($catA->id, 1);
-    $client->addToCart($catB->id, 2);
+    $client->addToCart('aaa', $catA->id, 1);
+    $client->addToCart('aaa', $catB->id, 2);
+    $client->payByCashBtn($client->placeOrder()); //TODO: this is giving errors: Needs review - Jul 18 2013
+    //$this->completeTransaction($client->placeOrder(false));
     
-    $this->completeTransaction($client->placeOrder(false));
+    $this->buyTickets('foo', 'bbb', $cat2->id, 5);
     
-    $this->buyTickets('foo', $cat2->id, 5);
-    
-    $this->buyTickets('foo', $catA->id, 9);
+    $this->buyTickets('foo', 'aaa', $catA->id, 9);
                       
     
     //$bar = $this->createUser('bar');
     $bar = $this->createUser('bar');
-    $this->buyTickets('bar', $cat3->id, 3);
+    $this->buyTickets('bar', 'ccc', $cat3->id, 3);
     
     //another google transaction
-    $this->buyTickets('foo', $catA->id, 5);
+    $this->buyTickets('foo', 'aaa', $catA->id, 5);
     
   }
  
