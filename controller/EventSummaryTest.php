f01831b022585ac38e83cefdcb58c07ef699249d
irodriguez <irodriguez@mobi360.ca>
2013-08-15 11:44:14 -0500

    2013 dates

--- controller/EventSummaryTest.php
+++ controller/EventSummaryTest.php
@@ -13,22 +13,22 @@ class EventSummaryTest extends DatabaseBaseTest{
 
     //Events
     $user_id = $this->addUser('seller');
-    $evt = $this->createEvent('First', $user_id, 1, '2012-01-01', '9:00', '2012-01-05', '18:00' );
+    $evt = $this->createEvent('First', $user_id, 1, '2013-01-01', '9:00', '2013-01-05', '18:00' );
     $this->setEventId($evt, 'aaa');
     $catA = $this->createCategory('CatA', $evt->id, 10.00);
     $catB = $this->createCategory('CatB', $evt->id, 4.00);
     
     $user_id = $this->addUser('seller2');
-    $evt = $this->createEvent('Second', $user_id, 1, '2012-02-01', '9:00', '2012-02-05', '18:00' );
+    $evt = $this->createEvent('Second', $user_id, 1, '2013-02-01', '9:00', '2013-02-05', '18:00' );
     $this->setEventId($evt, 'bbb');
     $cat2 = $this->createCategory('Cat2', $evt->id, 15.00);
     
     $user_id = $this->addUser('seller3');
-    $evt = $this->createEvent('Third', $user_id, 1, '2012-03-01', '9:00', '2012-03-05', '18:00' );
+    $evt = $this->createEvent('Third', $user_id, 1, '2013-03-01', '9:00', '2013-03-05', '18:00' );
     $this->setEventId($evt, 'ccc');
     $cat3 = $this->createCategory('Cat3', $evt->id, 20.00);
     
-    $evt = $this->createEvent('Cuarto', 'seller', 1, '2012-04-01');
+    $evt = $this->createEvent('Cuarto', 'seller', 1, '2013-04-01');
     $this->setEventId($evt, 'cuatro');
     $cat4 = $this->createCategory('Cat4', $evt->id, 10.00);
     $cat41 = $this->createCategory('Cat41', $evt->id, 12.00);
