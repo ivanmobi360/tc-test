f32c84f1d22d0fb045519ed7c215e1ca5af8adde
Ivan <irodriguez@mobi360.ca>
2014-05-13 10:08:09 -0500

    template test

--- controller/TemplateTest.php
+++ controller/TemplateTest.php
@@ -40,6 +40,49 @@ class TemplateTest extends \DatabaseBaseTest{
       $this->assertEquals(50, $cat['order']);
   }
   
+  /**
+   * we need to show a new input text field for each category called "Display Order" (or maybe just "Order" to make it short)
+   * that will be prefilled with the number 50 at creation and will show the actual `order` field at edition
+   */
+  function testOrder(){
+      $this->clearAll();
+      $seller = $this->createUser('seller');
+      $this->createUser('foo');
+      $loc = $this->createLocation('Quito');
+  
+  
+      Utils::clearLog();
+      $eb = \TemplateBuilder::createInstance($this, $seller)
+      ->id('aaa')
+      ->info('Martes Loco', $this->createVenue('Pool'), '10:00', '3:00')
+      ->addCategory(\CategoryBuilder::newInstance('Test 10', 100.00)->param('order', 10), $catA)
+      ->addCategory(\CategoryBuilder::newInstance('Test 30' , 300.00)->param('order', 30), $catB)
+      ->addCategory(\CategoryBuilder::newInstance('Test 20' , 200.00)->param('order', 20), $catB)
+      ;
+      $evt = $eb->create();
+  
+      //Expect an event
+      $this->assertRows(1, 'event');
+  
+      $expected = [
+      ['name' => 'Test 10', 'order' => 10]
+      , ['name' => 'Test 20', 'order' => 20]
+      , ['name' => 'Test 30', 'order' => 30]
+      ];
+  
+      $res = $this->db->getIterator("SELECT name, `order` FROM category WHERE event_id=? ORDER BY `order`", $evt->id);
+      foreach ($res as $i=> $cat){
+          $this->assertEquals($expected[$i]['name'], $cat['name']);
+          $this->assertEquals($expected[$i]['order'], $cat['order']);
+      }
+  
+      \ModuleHelper::showEventInAll($this->db, $evt->id, true);
+      
+      //needs to create some tour
+      
+  
+  }
+  
   
  
 }
